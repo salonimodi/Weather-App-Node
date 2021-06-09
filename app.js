f152015b1f54c1e7972d3da3adca50c509ffc151
saloni modi <salonimodi293@gmail.com>
2021-06-09 23:58:44 +0530

    WIP

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const request = require('request');
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const fs =  require('fs');
 
 
 rootPath = __dirname;
@@ -13,21 +14,68 @@ const port = process.env.PORT || 3000;
 app.set('view engine','hbs');
 app.use(express.static(rootPath));
 
-const url =
- "http://api.weatherstack.com/current?access_key=1dc13e5549f96974e595acb899a344ed&query=New York"
-request({url: url, json : true}, (error, response)=> {
-   const data = response.body.current;
+// const url =
+//  "http://api.weatherstack.com/current?access_key=1dc13e5549f96974e595acb899a344ed&query=New York"
+// request({url: url, json : true}, (error, response)=> {
+//    const data = response.body.current;
 
-   console.log(`It is currently ${data.wind_degree} out`);
-}) 
+//    console.log(`It is currently ${data.wind_degree} out`);
+// }) 
 
-app.get('',(req, res)=> {
-   // res.sendFile(rootPath + '/app.html');
-  // res.render('app');
-  console.log()
-  res.sendFile(path.join(__dirname, './views/app.hbs'));
+app.get('/', (req, res) => {
+
+   fs.readFile('./views/app.hbs', (err, resp) => {
+      if (err) {
+         res.writeHead(404, {
+            'Content-Type': 'text/pain'
+         });
+         res.end();
+      } else if (resp) {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.write(resp);
+      }
+      res.end();
+   })
+
 })
 
+app.get('/about/',(req, res)=> {
+   fs.readFile('./views/about.hbs', (err, resp) => {
+      if (err) {
+         res.writeHead(404, {
+            'Content-Type': 'text/pain'
+         });
+         res.end();
+      } else if (resp) {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.write(resp);
+      }
+      res.end();
+   })
+
+})
+
+app.get('/contact/',(req, res)=> {
+   fs.readFile('./views/contact.hbs', (err, resp) => {
+      if (err) {
+         res.writeHead(404, {
+            'Content-Type': 'text/pain'
+         });
+         res.end();
+      } else if (resp) {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.write(resp);
+      }
+      res.end();
+   })
+
+})
 
 app.listen(port, () => {
    console.log("running from "+ port+" server");
